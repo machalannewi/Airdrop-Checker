5e5560ce71b135342f12913a77957a3cc3b92363
Daniel Okeke <danielekene68@gmail.com>
2025-05-18 22:52:24 +0100

    Update

--- dashboard-app/src/main.jsx
+++ dashboard-app/src/main.jsx
@@ -13,10 +13,10 @@ const router = createBrowserRouter([
     path: '/',
     element: <App />, // Your main app component
     children: [
-        { index: true, element: <Register /> },
+        { index: true, element: <Login /> },
         { path: 'dashboard', element: <UserDashboardLayout /> },
         { path: 'login', element: <Login /> },
-        // { path: 'register', element: <Register /> },
+        { path: 'register', element: <Register /> },
     ],
   }
 ])
